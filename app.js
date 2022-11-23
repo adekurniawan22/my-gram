635c78ad565d243bd9123621a695277750c224cd
Ade Kurniawan <ade.kurniawan216@gmail.com>
2022-11-24 00:30:01 +0700

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const userRoute = require("./routers/userRoute");
 const photoRoute = require("./routers/photoRoute");
 const socialMediaRoute = require("./routers/socialMediaRoute");
 const commentRoute = require("./routers/commentRoute");
-const PORT = 5000;
+const PORT = process.env.PGPORT || 80;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
